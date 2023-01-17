3a641c389814ee9742ccdf1e0783bbcb43656fda
fdavidsen <f.davidsen19@gmail.com>
2023-01-17 20:53:53 +0700

    Add route for sample data

--- Backend/server.js
+++ Backend/server.js
@@ -3,7 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const port = process.env.PORT;
+const port = process.env.PORT || 8080;
+
+const devRoutes = require("./routes/sample-data");
 
 app.use(express.json());
 app.use(cors());
@@ -12,3 +14,5 @@ app.use(express.urlencoded({ extended: true }));
 app.listen(port, () => {
   console.log(`Server Started at ${port}`);
 });
+
+app.use("/dev", devRoutes);
