5a1cb48f48aecd4526d8691674424d151380bc66
R10F <farrellriofa@gmail.com>
2023-01-24 16:40:54 +0700

    update data

--- Backend/app/models/data.sample.js
+++ Backend/app/models/data.sample.js
@@ -1,11 +1,11 @@
 const toko = [
   {
-    nama: "Sinar Rezeki Store",
-    kota: "Tangerang Selatan",
+    nama: "Kahf Official",
+    kota: "Jakarta Selatan",
     logo: "",
   },
   {
-    nama: "Sinar Rezeki Store",
+    nama: "Frisian Flag Official",
     kota: "Tangerang Selatan",
     logo: "",
   },
@@ -13,36 +13,36 @@ const toko = [
 
 const produk = [
   {
-    nama: "Yupi Strawberry Kiss 120 gr",
-    harga: 8975,
+    nama: "Kahf Oil and Acne Care Face Wash 100 ml - Sabun Wajah Pria",
+    harga: 40700,
     qty: 1,
-    diskon: 0,
+    diskon: 20,
     gambar:
-      "https://images.tokopedia.net/img/cache/900/VqbcmM/2022/7/8/c8158a70-3b3b-4e8a-ad8e-ac154c63251e.jpg",
+      "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/12/23/946483af-8df4-4b2d-8e82-b4bca9e1dd60.png",
+  },
+  {
+    nama: "Kahf Gentle Exfoliating Face Scrub 100 ml - Lawan Noda Hitam",
+    harga: 40700,
+    qty: 1,
+    diskon: 20,
+    gambar:
+      "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/12/23/1455347b-a711-4d57-8f07-9c446fe61d7d.png",
   },
   {
     nama: "Frisian Flag Milky Susu UHT Chocolate 115ml [12 pcs]",
-    harga: 35000,
+    harga: 40400,
     qty: 1,
-    diskon: 5,
+    diskon: 18,
     gambar:
       "https://images.tokopedia.net/img/cache/900/VqbcmM/2022/7/8/c8158a70-3b3b-4e8a-ad8e-ac154c63251e.jpg",
   },
   {
-    nama: "Yupi Strawberry Kiss 120 gr",
-    harga: 8975,
+    nama: "Frisian Flag Purefarm Susu Bubuk Keluarga Fullcream [2x800g] Free Gift - Tempat Pensil",
+    harga: 233600,
     qty: 1,
-    diskon: 0,
+    diskon: 20,
     gambar:
-      "https://images.tokopedia.net/img/cache/900/VqbcmM/2022/7/8/c8158a70-3b3b-4e8a-ad8e-ac154c63251e.jpg",
-  },
-  {
-    nama: "Yupi Strawberry Kiss 120 gr",
-    harga: 8975,
-    qty: 1,
-    diskon: 15,
-    gambar:
-      "https://images.tokopedia.net/img/cache/900/VqbcmM/2022/7/8/c8158a70-3b3b-4e8a-ad8e-ac154c63251e.jpg",
+      "https://images.tokopedia.net/img/cache/900/VqbcmM/2022/12/28/86829634-81e4-444b-8ed7-e19a38d77a5c.jpg",
   },
 ];
 
